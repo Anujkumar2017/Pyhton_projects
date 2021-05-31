9f8ea82e98398c0e6af92029cbbfab1b0b9bbf89
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2021-05-31 22:44:06 +0530

    Delete speedTestP1.py

--- MiniProjects/speedTestP1.py
+++
@@ -1,9 +0,0 @@
-from speedtest import Speedtest
-
-#creating an object 
-st = Speedtest()
-try:
-    print(f"Downloading speed: {st.download()}kb\s")
-    print(f"Uploading speed: {st.upload()}kb\s")
-except Exception:
-    print("Error!.. Please check internet connection")
